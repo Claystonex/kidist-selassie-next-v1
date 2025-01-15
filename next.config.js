ad7440e44426d15c8c757dd6c00356b34e416bda
Apple <hunggarchristian@gmail.com>
2025-01-14 20:37:36 -0800

    Updated next configur

--- next.config.js
+++ next.config.js
@@ -2,20 +2,24 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-// import "./src/env.js";
+import "./src/env.js";
 
-// /** @type {import("next").NextConfig} */
-// const config = {};
-
-// export default config;
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/** @type {import("next").NextConfig} */
+const config = {
     images: {
-      domains: ['img.clerk.com', 'images.clerk.dev'],
+        domains: ['img.clerk.com', 'images.clerk.dev'],
     },
     reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
+};
+
+export default config;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       domains: ['img.clerk.com', 'images.clerk.dev'],
+//     },
+//     reactStrictMode: true,
+//   };
+
